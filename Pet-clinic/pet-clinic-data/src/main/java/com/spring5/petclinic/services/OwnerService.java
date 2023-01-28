66b6ca31bc4c14d245059ea21d0168447271b11d
VinhLB <dongoc409@gmail.com>
2023-01-28 23:40:40 +0700

    Create Interfaces for Pet Clinic Services #5

---
+++ Pet-clinic/pet-clinic-data/src/main/java/com/spring5/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.spring5.petclinic.services;
+
+import com.spring5.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastname(String lastname);
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
