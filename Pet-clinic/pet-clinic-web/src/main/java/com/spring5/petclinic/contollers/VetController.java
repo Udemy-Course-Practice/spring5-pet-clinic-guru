9c801e21b9e5d1d643cc16cba0da61c30fc40713
VinhLB1997 <43948809+VinhLB1997@users.noreply.github.com>
2023-01-31 11:12:07 +0700

    refactor url

--- Pet-clinic/pet-clinic-web/src/main/java/com/spring5/petclinic/contollers/VetController.java
+++ Pet-clinic/pet-clinic-web/src/main/java/com/spring5/petclinic/contollers/VetController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Set;
 
 @Controller
-@RequestMapping("/vets")
+@RequestMapping({"/vets","/vets.html"})
 public class VetController {
 
     private final VetService vetService;
