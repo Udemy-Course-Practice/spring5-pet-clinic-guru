47d39b753f82f7f65d4cf6957f04abbb8922a9e8
VinhLB1997 <43948809+VinhLB1997@users.noreply.github.com>
2023-01-31 09:36:22 +0700

    Create Vet Speciality Entity, Associate To Vet. Closes #25

--- Pet-clinic/pet-clinic-data/src/main/java/com/spring5/petclinic/model/Vet.java
+++ Pet-clinic/pet-clinic-data/src/main/java/com/spring5/petclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package com.spring5.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    Set<Speciality> specialitys;
+
+    public Set<Speciality> getSpecialitys() {
+        return specialitys;
+    }
+
+    public void setSpecialitys(Set<Speciality> specialitys) {
+        this.specialitys = specialitys;
+    }
 }
